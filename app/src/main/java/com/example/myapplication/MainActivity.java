53ec71d8222dc4070bc0940d69fce1f8518a3dc7
just1night <fuihikaru1@gmail.com>
2024-03-06 17:28:16 +0700

    update login and register
    id: editTextTextAcc
         editTextTextAcc1
         editTextTextPassword
         editTextTextPassword1
         editTextTextRePassword
         buttonlogin1
         buttonregister1
         buttonregister2
    -layout: ConstrainLayout+LinearLayout
    - add button color folder in res
    -add log_regi_box in drawable
    
    not yet connect to home
    not yet connect to database myphp
    
    need to find way to connect throw the 3rd web

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText acc = findViewById(R.id.editTextTextAcc);
+        EditText pass = findViewById(R.id.editTextTextPassword);
+
+        Button reg1 = findViewById(R.id.buttonResigter1);
+        Button log = findViewById(R.id.buttonLogin1);
+        reg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(MainActivity.this,RegisterAct.class);
+                startActivity(i);
+            }
+        });
+        log.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent i = new Intent();
+                i .setClass(MainActivity.this, HomeAct.class);
+                startActivity(i);
+            }
+        });
+
+
     }
+    @Override
+    protected void onPause() {
+        super.onPause();}
 }
